e50362e8ebd1819eea9197c75bfbed4be8aa995d
victorOS16 <victordaniel160900@outlook.com>
2022-06-03 16:07:53 -0300

    Adição do projeto de DOM

---
+++ DOM/assets/js/script.js
@@ -0,0 +1,45 @@
+/* function changeMode() {
+	changeClasses();
+	changeText();
+}
+
+function changeClasses() {
+	button.classList.toggle(darkModeClass);
+	h1.classList.toggle(darkModeClass);
+	body.classList.toggle(darkModeClass);
+	footer.classList.toggle(darkModeClass);
+}
+
+function changeText() {
+	const lightMode = 'Light Mode';
+	const darkMode = 'Dark Mode';
+
+	if (body.classList.contains(darkModeClass)) {
+		button.innerHTML = lightMode;
+		h1.innerHTML = darkMode + ' ON';
+		return;
+	}
+
+	button.innerHTML = darkMode;
+	h1.innerHTML = lightMode + ' ON';
+}
+
+const darkModeClass = 'dark-mode';
+const button = document.getElementById('mode-selector');
+const h1 = document.getElementById('page-title');
+const body = document.getElementsByTagName('body')[0];
+const footer = document.getElementsByTagName('footer')[0];
+
+button.addEventListener('click', changeMode); */
+let qtdEntradas = 3
+let melhor = 999
+
+
+    
+    let arr = [9.82, 9.72, 9.92]
+    arr.sort()
+    melhor = arr[0]
+    //if (tempo) melhor = 
+        	
+
+console.log(melhor)
